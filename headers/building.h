f25538a6ffdebef623277b84157cbe1ea1a8b8ba
tfgungun <gunawan48160@gmail.com>
2019-10-22 02:19:05 +0700

    Add base header for building

---
+++ headers/building.h
@@ -0,0 +1,156 @@
+/* File : building.h */
+/* Definisi ADT Building dengan representasi array secara implisit dan alokasi dinamik */
+/* Elemen Building terurut berdasarkan nomor building */
+
+#ifndef _building_H
+#define _building_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+#include "arraydin.h"
+#include "point.h"
+
+/* Definisi elemen Building */
+typedef struct {
+    int kind;  /* [1..4] Menandakan jenis bangunan */
+    		   /* 1 = Castle
+    		      2 = Tower
+    		      3 = Fort
+    		      4 = Village */
+    int player;  /* [0..2] Menandakan player mana yang memiliki bangunan */
+                 /* 0 = No Player
+                    1 = Player 1
+                    2 = Player 2 */
+    int level; /* [0..4] Level dari bangunan */
+    int soldierCount; /* Jumlah pasukan yang ada di bangunan */
+    int soldierAddValue; /* A = Nilai penambahan pasukan */
+    int maximumSoldierCount; /* M = Batas atas jumlah pasukan di bangunan */
+    boolean isDefended; /* Boolean untuk menandakan apakah bangunan dilindungi */ 
+    POINT position; /* Posisi bangunan dalam map */
+} Building;
+
+/* Definisi List Building dengan implementasi array dinamik*/
+typedef struct {
+    Building * T;   /* tabel penyimpan elemen */
+    int NBElmt;		/* Jumlah elemen list sekarang */
+    int MaxEl;     /* Max elemen list */
+} BuildingList;
+
+/* ********* AKSES (Selektor) ********* */
+/* Jika B adalah Building, maka akses elemen : */
+#define kind(B)    				(B).kind
+#define player(B)   			(B).player
+#define level(B)     			(B).level
+#define soldierCount(B)     	(B).soldierCount
+#define soldierAddValue(B)     	(B).soldierAddValue
+#define maximumSoldierCount(B)	(B).maximumSoldierCount
+#define isDefended(B)     		(B).isDefended
+#define position(B)     		(B).position
+
+/* ********* Prototype ********* */
+int NBElmt (BuildingList BL);
+/* Mengirimkan banyaknya jumlah building sekarang */
+
+int MaxElmt (BuildingList BL);
+/* Mengirimkan maksimum banyaknya jumlah building sekarang */
+
+Building BuildingElmt (BuildingList BL, int index);
+/* Mengirimkan building ke-index dari BL */
+
+int UForBuilding (int kind);
+/* Mengirimkan nilai U untuk kind tertentu */
+
+int AForBulding (int kind, int level);
+/* Mengirimkan nilai A untuk kind dan level tertentu */
+
+int MForBuilding (int kind, int level);
+/* Mengirimkan nilai M untuk kind dan level tertentu */
+
+boolean PForBuilding (int kind, int level);
+/* Mengirimkan nilai P untuk kind dan level tertentu */
+
+/* *** Kreator *** */
+void CreateEmptyBuildingList (BuildingList * BL, int Max);
+/* I.S. sembarang */
+/* F.S. Sebuah BL kosong terbentuk dan salah satu kondisi sbb: */
+/* Jika alokasi berhasil, Tabel memori dialokasikan tipe Building berukuran Max+1 */
+/* atau : jika alokasi gagal, BL kosong dg MaxEl=0 */
+/* Proses : Melakukan alokasi, membuat sebuah BL kosong */
+
+void CreateBuilding (BuildingList * BL, int kind, int player, POINT position);
+/* I.S. sembarang atau tidak penuh */
+/* F.S. Elemen BL bertembah dengan bangunan bertipe kind di posisi position */
+/* Proses : Melakukan penambahan bangunan pada NBElmt + 1 dan */
+/* Set kind dan position sesuai parameter */
+
+void InitializeBuilding(Building *B);
+/* I.S. Atribut B yang telah diset hanya kind, player, dan position */
+/* F.S. Atribut Elemen B lengkap dengan spesifikasi */
+/* level = 0
+   soldierCount = U
+   soldierAddValue = A
+   maximumSoldierCount = M
+   isDefended = P
+*/
+/* Proses : Melakukan setup sesuai kind */
+
+/* *** Destruktor *** */
+void DeAlokasi(BuildingList * BL);
+/* Proses: Mengembalikan memori BL */
+/* I.S. BL pernah dialokasi */
+/* F.S. BL menjadi tidak terdefinisi lagi, MaxEl dan NBElmt diset 0 */
+
+/* *** Primitif *** */
+String GetBuildingName(int kind);
+/* Mengirimkan nama building dengan kind tertentu */
+
+void IncreasePasukan (BuildingList * BL, int index, int value);
+/* I.S. Jumlah pasukan bangunan ke-index BL sudah terdefinisi */
+/* F.S. Jumlah pasukan bertambah sesuai parameter value */
+/* Proses: Menambahkan value pada soldierCount dari Building */
+/* Jika jumlah akhir melebihi jumlah pasukan yang dapat ditampung,
+   soldierCount akan sama dengan M */
+
+void DecreasePasukan (BuildingList * BL, int index, int value);
+/* I.S. Jumlah pasukan bangunan ke-index BL sudah terdefinisi */
+/* F.S. Jumlah pasukan berkurang sesuai parameter value */
+/* Proses: Mengurangkan value pada soldierCount dari Building */
+/* Jika jumlah akhir kurang dari 0, soldierCount akan menjadi 0 */
+
+void IncreaseAllPasukan (BuildingList * BL);
+/* I.S. Jumlah pasukan bangunan ke-index BL sudah terdefinisi */
+/* F.S. Jumlah pasukan bertambah sesuai nilai atribut A dari masing-masing building */
+/* Proses: Menambahkan nilai atribut A pada soldierCount dari tiap Building */
+
+void ChangePlayer (BuildingList * BL, int index);
+/* I.S. Bangunan ke-index BL terdefinisi milik salah satu player */
+/* F.S. Bangunan ke-index BL berubah kepemilikan menjadi player yang lain */
+/* Proses: Mengganti nilai atribut player pada Building dan kembali inisialisasi Building. */
+
+void LevelUpBuilding (BuildingList * BL, int index);
+/* I.S. Bangunan ke-index BL sudah terdefinisi*/
+/* F.S. Atribut building akan menjadi sbb: */
+/* Level bertambah 1 dan soldierCount berkurang sebesar M/2 */
+/* Atau : Level tetap karena sudah maksimum atau soldierCount kurang (berikan prompt) */
+/* Proses: Mengecek apakah levelup bisa dilakukan, jika bisa FS1 jika tidak FS2 */
+
+/* Operasi Tambahan */
+void PrintBuildingInfo (BuildingList BL, int index);
+/* Mencetak isi info Building ke-index ke layar */
+/* I.S. BL dan B terdefinisi */
+/* F.S. B tercetak ke layar dengan format:
+<Nama Bangunan> <(Posisi)> <Jumlah Pasukan> <Level>
+*/
+
+void PrintBuildingList (BuildingList BL);
+/* Mencetak isi info semua Building di BuildingList ke layar */
+/* I.S. BL */
+/* F.S. BL tercetak ke layar dengan format:
+<index> <Nama Bangunan> <(Posisi)> <Jumlah Pasukan> <Level>
+....
+....
+*/
+
+#endif
