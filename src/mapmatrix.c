0b98fdbaeacb1cd1eed8a8018ac108330e30b8c8
nadiaphi <53868722+nadiaphi@users.noreply.github.com>
2019-11-06 17:51:23 +0700

    Add files via upload

---
+++ src/mapmatrix.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pcolor.h"
+#include "boolean.h"
+#include "point.h"
+#include "buildingarray.h"
+
+/*#include "matrix.h"
+///////////////////////////////////
+//		  CONST & TYPEDEF	 	 //
+///////////////////////////////////
+/* Ukuran minimum dan maksimum baris dan kolom */
+#define MapMatrixBarisMinimum 1
+
+#define MapMatrixBarisMaximum 30
+
+#define MapMatrixKolomMinimimum 1
+
+#define MapMatrixKolomMaximum 20
+
+//////////////////////////
+//        SELECTOR      //
+//////////////////////////
+/* ********* AKSES (Selektor) ********* */
+/* Jika M adalah Matrix dan P adalah Point, maka akses elemen : */
+#define MapMatrixNBrsEff(M) 		(M).NBrsEff
+#define MapMatrixNKolEff(M) 		(M).NKolEff
+#define MapMatrixElement(M,P) 		(M).Mem[(PointX(P))][(PointY(P))]
+
+///////////////////////////
+// 		CONSTRUCTOR 	 //
+///////////////////////////
+void MapMatrixCreateEmpty(MapMatrix *M, int row, int col){
+    MapMatrixNBrsEff(*M) = row;
+	MapMatrixNKolEff(*M) = col;
+	int i,j;
+	for (i = 1; i <= row; i++)
+		for (j = 1; j <= col; j++)
+			Elmt(*M,i,j) = 0;
+    /*
+    MatrixCreateEmpty(*M, row, col); 
+    */
+}
+/* Membentuk sebuah MapMatrix "kosong" yang siap diisi berukuran row x column di "ujung kiri" memori */
+/* I.S. row dan col adalah valid untuk memori matriks yang dibuat */
+/* F.S. Matrix M sesuai dengan definisi di atas terbentuk
+		row dan col menjadi NBrsEff dan NKolEff */
+
+///////////////////////////
+// 		 PREDIKAT		 //
+///////////////////////////
+boolean IsMapMatrixIdxValid(MapMatrixIdxType index){
+    return index >= MapMatrixBarisMinimum && index <= MapMatrixBarisMaximum && index >= MapMatrixKolomMinimimum && index <= MapMatrixKolomMaximum;
+}
+/* Mengirimkan true jika PointX(index), PointY(index) adalah indeks yang valid untuk matrix apa pun */
+
+boolean IsMapMatrixIdxEff(MapMatrix M, MapMatrixIdxType index){
+    return ((MapMatrixBarisMinimum<=index) && (index<=MapMatrixNBrsEff(M)) && ((MapMatrixKolomMinimimum<=index) && (index<=MapMatrixNKolEff(M))));
+}
+/* Mengirimkan true jika PointX(index), PointY(index) adalah indeks efektif bagi M */
+
+///////////////////////////////////
+// 	    MAP MATRIX OPERATIONS 	 //
+///////////////////////////////////
+int MapMatrixNBElmt(MapMatrix M){
+    return MapMatrixNBrsEff(M) * MapMatrixNKolEff(M);
+    /*
+    NBElmt(M);
+    */
+}
+/* Mengirimkan banyaknya elemen M */
+
+int MapMatrixFirstIdxBaris(MapMatrix M){
+    return MapMatrixBarisMinimum;
+}
+/* Mengirimkan indeks baris terkecil M */
+
+int MapMatrixFirstIdxKolom(MapMatrix M){
+    return MapMatrixKolomMinimimum;
+}
+/* Mengirimkan indeks kolom terkecil M */
+
+int MapMatrixLastIdxBaris(MapMatrix M){
+    return MapMatrixNBrsEff(M);
+}
+/* Mengirimkan indeks baris terbesar M */
+
+int MapMatrixLastIdxKolom(MapMatrix M){
+    return MapMatrixNKolEff(M);
+}
+/* Mengirimkan indeks kolom terbesar M */
+
+void MapMatrixPrintMap(MapMatrix M);
+/* I.S. M terdefinisi */
+/* F.S. Map dicetak ke layar dengan format 
+*********
+*X		*
+*		*
+*		*
+*	   Y*
+*********
+dibatasi boundary * dengan jumlah NKolEff + 2 untuk atas bawah dan NBrsEff + 2 untuk kanan kiri
+X dan Y adalah char bangunan yang dimiliki atau tidak dimiliki pemain 1 atau 2
+Jika sebuah bangunan dimiliki pemain, pencetakan char dilakukan dengan warna
+Tidak ada spasi antara bangunan 1 dengan yang lain
+Ada enter di setiap akhir baris 
+*/
+
+Building MapMatrixGetBuilding(MapMatrix M, MapMatrixIdxType pos, BuildingArray T);
+/* Mengembalikan building pada point pos */
