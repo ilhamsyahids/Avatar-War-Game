584355f2a95390867e41589f5b7a5f4b07250466
Ilham Syahid S <ilhamsyahids@gmail.com>
2019-11-15 16:36:05 +0700

    add GameMap

--- src/gamemap.c
+++ src/gamemap.c
@@ -3,14 +3,11 @@
 //					GameMap             		//
 //                                     			//
 //----------------------------------------------//
-// File : gamemap.h */              			//
+// File : gamemap.c */              			//
 //----------------------------------------------//
 // Definisi ADT GameMap yang menghubungkan		//
 // MapMatrix dengan BuildingArray               //
 //==============================================//
-
-#ifndef _GAMEMAP_H
-#define _GAMEMAP_H
 
 ///////////////////////////
 //		  INCLUDE	 	 //
@@ -22,49 +19,38 @@
 #include "buildingarray.h"
 #include "player.h"
 #include "buildingrelationgraph.h"
-
-///////////////////////////////////
-//		  CONST & TYPEDEF	 	 //
-///////////////////////////////////
-
-///////////////////////////////
-// 		Definisi GameMap  	 //
-///////////////////////////////
-typedef struct
-{
-	MapMatrix BuildingMap;					/* Matrix peta untuk bangunan */
-	BuildingArray BuildingRecord;			/* Daftar seluruh bangunan di peta */
-	Player CurrentPlayer;					/* Player yang sedang menjalankan turn saat itu */
-	BuildingRelationGraph BuildingRelation; /* Relasi antar bangunan pada peta */
-} GameMap;
-
-//////////////////////////
-//        SELECTOR      //
-//////////////////////////
-/* ********* AKSES (Selektor) ********* */
-/* Jika M adalah GameMap, maka akses elemen : */
-#define BuildingMap(G) (G).BuildingMap
-#define BuildingRecord(G) (G).BuildingRecord
-#define CurrentPlayer(G) (G).CurrentPlayer
-#define BuildingRelation(G) (G).BuildingRelation
+#include "gamemap.h"
 
 ///////////////////////////
 // 		CONSTRUCTOR 	 //
 ///////////////////////////
-void GameMapCreate(GameMap *G, MapMatrix map, BuildingArray record, Player CurrentPlayer, BuildingRelationGraph BuildingRelation);
+void GameMapCreate(GameMap *G, MapMatrix map, BuildingArray record, Player CurrentPlayer, BuildingRelationGraph BuildingRelation)
 /* Membentuk sebuah GameMap yang berisi BuildingMap, BuildingRecord dan currentPlayer */
 /* I.S. map, record, dan currentPlayer diinisiasi di luar method sebelum dimasukkan sebagai parameter */
 /* F.S. Terbentuk GameMap yang berisi keadaan game pada saat itu*/
+{
+	BuildingMap(*G) = map;
+	BuildingRecord(*G) = record;
+	CurrentPlayer(*G) = CurrentPlayer;
+	BuildingRelation(*G) = BuildingRelation;
+}
 
 ///////////////////////////////////
 // 	    MAP MATRIX OPERATIONS 	 //
 ///////////////////////////////////
-void GameMapChangePlayer(GameMap *G);
+void GameMapChangePlayer(GameMap *G)
 /* Menukar currentPlayer dengan player yang lain */
 /* I.S currentPlayer GameMap adalah 1/2 */
 /* F.S curretntPlayer GameMap menjadi 2/1 */
+{
 
-void GameMapPrintInfo(GameMap G);
+	if (CurrentPlayer(*G).role == 1)
+		CurrentPlayer(*G).role = 2;
+	else
+		CurrentPlayer(*G).role = 1;
+}
+
+void GameMapPrintInfo(GameMap G)
 /* Mencetak info GameMap sebagai berikut */
 /* [MapMatrixPrintMap(BuildingMap(G))]
 	******
@@ -81,4 +67,7 @@ void GameMapPrintInfo(GameMap G);
 	Available Skill: IU
 
 */
-#endif
+{
+	MapMatrixPrintMap(BuildingMap(G));
+	BuildingListPrintInfo(PlayerOwnedBuildingList(CurrentPlayer(G)), BuildingRecord(G));
+}
