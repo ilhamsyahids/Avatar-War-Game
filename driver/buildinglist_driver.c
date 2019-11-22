f3ca4a8eab1be6e37616eaeab20e7eb9a2a09cc0
tfgungun <gunawan48160@gmail.com>
2019-11-22 14:46:27 +0700

    bug delete last buildinglist

--- driver/buildinglist_driver.c
+++ driver/buildinglist_driver.c
@@ -15,6 +15,7 @@ int main()
     int i;
     int j;
     Point P;
+    int X;
 
     BuildingListCreateEmpty(&L);
     if (IsBuildingListEmpty(L))
@@ -26,7 +27,7 @@ int main()
         MapMatrixCreateEmpty(&M, 3, 3);
         BuildingArrayCreateEmpty(&B, 3);
 
-        BB = BuildingCreate(1, 1, PointCreate(1, 1));
+        BB = BuildingCreate(2, 1, PointCreate(1, 1));
         BuildingArrayAddAsLastElement(&B, BB);
         MapMatrixElement(M, PointCreate(1, 1)) = 1;
         // printf("%d", MapMatrixElement(M, PointCreate(1, 1)));
@@ -41,18 +42,41 @@ int main()
 
         BuildingListInsertValueLast(&L, 2);
         BuildingListPrintInfo(L, B);
-        BuildingListDeleteValue(&L, 2);
+        //BuildingListDeleteValue(&L, 2);
         printf("\n");
         BuildingListPrintInfo(L, B);
         printf("\n");
         BuildingListInfotype x;
-        BuildingListDeleteValueFirst(&L, &x);
+        //BuildingListDeleteValueFirst(&L, &x);
         BuildingPrintInfo(BuildingArrayElement(B, x));
         printf("\n");
         BuildingListPrintInfo(L, B);
 
-        BuildingList LL = CopyBuildingList(L);
-        BuildingListPrintInfo(LL, B);
+        printf("Kind 1 : %d\n", BuildingListNbElmtKind(L, B, 1));
+        printf("Kind 2 : %d\n", BuildingListNbElmtKind(L, B, 2));
+        printf("Kind 3 : %d\n", BuildingListNbElmtKind(L, B, 3));
+        printf("Kind 4 : %d\n", BuildingListNbElmtKind(L, B, 4));
+
+        printf("Level 4 all : %d\n", IsBuildingListAllLevel4(L, B));
+
+        BuildingLevel(BuildingArrayElement(B, 1)) = 4;
+        BuildingLevel(BuildingArrayElement(B, 2)) = 4;
+        BuildingLevel(BuildingArrayElement(B, 3)) = 4; 
+
+        BuildingListPrintInfo(L, B);
+
+        printf("Level 4 all : %d\n", IsBuildingListAllLevel4(L, B));
+        
+        // di bawah ini ga lewat
+        BuildingListDeleteValueLast(&L, &X);
+        
+        //printf("1\n");
+        //BuildingListDeleteValueLast(&L, &X);
+        //printf("2\n");
+        //BuildingListDeleteValueLast(&L, &X);
+        //printf("3\n");
+        //BuildingListDeleteValueLast(&L, &X);
+        //printf("4\n");
     }
 
     return 0;
